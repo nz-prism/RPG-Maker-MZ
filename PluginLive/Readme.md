f59054981b611f3aa2228e439bbbe171081a08c0
nz-prism <yytcg721@yahoo.co.jp>
2022-06-05 18:16:20 +0900

    06.05.2022 Prepared for PluginLive

---
+++ PluginLive/Readme.md
@@ -0,0 +1,25 @@
+# プラグインテンプレート(PluginLive.js)
+
+## バージョン
+### 最新バージョン（ダウンロード）
+[1.0.0](https://raw.githubusercontent.com/nz-prism/RPG-Maker-MZ/master/DynamicEncounterWeight/js/plugins/PluginLive.js)
+
+### バージョン履歴
+- 2022/06/11 1.0.0 リリース
+
+## 概要
+これはマークダウンドキュメントのテンプレートです。
+
+![PluginLive](https://github.com/nz-prism/RPG-Maker-MZ/blob/master/ReadmeImages/PluginLive1.png)
+
+
+
+## ライセンス
+このプラグインはMITライセンスにてリリースされています。  
+https://opensource.org/licenses/mit-license.php
+
+
+## 使用方法
+導入方法は通常のプラグインと同様です。  
+**js/plugins**フォルダに**PluginLive.js**をコピーし、プラグイン管理にて有効化してください。  
+プラグイン説明にて本ドキュメントと同等の内容がご確認いただけます。
---
+++ PluginTemplateFull.js
@@ -0,0 +1,103 @@
+//=============================================================================
+// RPG Maker MZ - PluginTemplate
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @plugindesc It's a plugin template.
+ * @author 
+ * @url 
+ *
+ * @help PluginTemplate.js
+ * ver. 1.0.0
+ * 
+ * [History]
+ * 01/01/2022 1.0.0 Released
+ * 
+ * This is a plugin template.
+ *
+ * 
+ * This plugin is released under MIT license.
+ * https://opensource.org/licenses/mit-license.php
+ * 
+ *
+ * @param paramTemplate
+ * @text Parameter Template
+ * @desc It's a template for a plugin parameter.
+ * @default 0
+ * @type number
+ * 
+ * 
+ * @command commandTemplate
+ * @text Command Template
+ * @desc It's a template for a plugin command.
+ * 
+ * @arg commandArg
+ * @text Command Argument
+ * @desc It's an argument for the plugin command.
+ * @default 0
+ * @type select
+ * @option Value Template1
+ * @value 0
+ * @option Value Template2
+ * @value 1
+ * 
+ */
+
+/*:ja
+ * @target MZ
+ * @plugindesc プラグインのテンプレートです。
+ * @author 
+ * @url 
+ *
+ * @help PluginTemplate.js
+ * ver. 1.0.0
+ * 
+ * [バージョン履歴]
+ * 2022/01/01 1.0.0 リリース
+ * 
+ * これはプラグインのテンプレートです。
+ * 
+ * 
+ * このプラグインはMITライセンスにてリリースされています。
+ * https://opensource.org/licenses/mit-license.php
+ *
+ * 
+ * @param paramTemplate
+ * @text パラメータテンプレート
+ * @desc プラグインパラメータのテンプレートです。
+ * @default 0
+ * @type number
+ * 
+ * 
+ * @command commandTemplate
+ * @text コマンドテンプレート
+ * @desc プラグインコマンドのテンプレートです。
+ * 
+ * @arg commandArg
+ * @text コマンドテンプレート引数
+ * @desc コマンドテンプレートの引数です。
+ * @default 0
+ * @type select
+ * @option 値テンプレート1
+ * @value 0
+ * @option 値テンプレート2
+ * @value 1
+ * 
+ */
+
+(() => {
+    'use strict';
+    const PLUGIN_NAME = "PluginTemplate";
+    const pluginParams = PluginManager.parameters(PLUGIN_NAME);
+
+    const PARAM_TEMPLATE = Number(pluginParams.paramTemplate);
+
+
+    PluginManager.registerCommand(PLUGIN_NAME, "commandTemplate", args => {
+        Number(args.commandArg);
+    });
+
+
+
+})();
